b9610ae2d628555156c9888e3ab7479b9c866ef4
skipperGo <ionut.saftoiu@student.upt.ro>
2023-11-11 17:38:14 +0200

    making the function

---
+++ proiect-sincretic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void hanoiTowers(int n, int src, int dest, int aux)
+{
+  if (n == 1)
+    cout << "Move disk 1 from tower " << src << " to tower " << dest << endl;
+}
+
+int main()
+{
+}
