26647b1486662ad88d7cc4713d3ebe9ac7334d40
skipperGo <ionut.saftoiu@student.upt.ro>
2023-11-11 17:43:27 +0200

    Update proiect-sincretic.cpp

--- proiect-sincretic.cpp
+++ proiect-sincretic.cpp
@@ -5,7 +5,14 @@ using namespace std;
 void hanoiTowers(int n, int src, int dest, int aux)
 {
   if (n == 1)
+  {
     cout << "Move disk 1 from tower " << src << " to tower " << dest << endl;
+    return;
+  }
+
+  hanoiTowers(n - 1, src, aux, dest);
+  cout << "Move disk " << n << "from tower" << src << "to tower" << dest << endl;
+  hanoiTowers(n - 1, aux, dest, src);
 }
 
 int main()
